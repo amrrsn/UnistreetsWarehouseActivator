5db29930f37fc0a64c450584c9d34c07a169c17c
YellowZombie <bumblebeealan01@gmail.com>
2020-08-09 17:13:42 -0400

    Changed to MIT License

--- UnistreetsWarehouseActivator.user.js
+++ UnistreetsWarehouseActivator.user.js
@@ -2,7 +2,7 @@
 // @name         UnistreetsWarehouseActivator
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @license      GPLV3
+// @license      MIT
 // @description  Activates Every Warehouse Without Reloads
 // @author       SomeDude
 // @match        https://www.unistreets.com/warehouse.php
