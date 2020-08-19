edfe8bc4cec42903feaec33605325d8fdcd1fcad
YellowZombie <bumblebeealan01@gmail.com>
2020-08-18 23:37:00 -0400

    Added Check to Prevent Sending Extra Data

--- UnistreetsWarehouseActivator.user.js
+++ UnistreetsWarehouseActivator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         UnistreetsWarehouseActivator
 // @description  Activates Every Warehouse Without Reloads
-// @version      0.4
+// @version      0.5
 // @license      MIT
 // @author       SomeDude
 // @grant        none
@@ -22,23 +22,25 @@
             var numItems = $('.evens').length + $('.odds').length;
             
             for (var i = 1; i <= numItems; i++) {
-                fetch("https://www.unistreets.com/warehouse.php?action=start&MID=" + i, {
-                    "headers": {
-                        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                        "accept-language": "en-US,en;q=0.9",
-                        "cache-control": "max-age=0",
-                        "content-type": "application/x-www-form-urlencoded",
-                        "sec-fetch-dest": "document",
-                        "sec-fetch-mode": "navigate",
-                        "sec-fetch-site": "same-origin",
-                        "sec-fetch-user": "?1",
-                        "upgrade-insecure-requests": "1",
-                        "cookie": cookieMonster
-                    },
-                    "body": "Start=Start",
-                    "method": "POST",
-                    "mode": "cors"
-                });            
+                if (!$("b").is("#timer-" + i)) {
+                    fetch("https://www.unistreets.com/warehouse.php?action=start&MID=" + i, {
+                        "headers": {
+                            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                            "accept-language": "en-US,en;q=0.9",
+                            "cache-control": "max-age=0",
+                            "content-type": "application/x-www-form-urlencoded",
+                            "sec-fetch-dest": "document",
+                            "sec-fetch-mode": "navigate",
+                            "sec-fetch-site": "same-origin",
+                            "sec-fetch-user": "?1",
+                            "upgrade-insecure-requests": "1",
+                            "cookie": cookieMonster
+                        },
+                        "body": "Start=Start",
+                        "method": "POST",
+                        "mode": "cors"
+                    });
+                }    
             }
             $('.general_btn').remove();
         }
